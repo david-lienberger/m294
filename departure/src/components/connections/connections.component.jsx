79da4a3e269c6e01803f256ac481fe96d2d3887f
e536367 <david.lienberger@sbb.ch>
2023-05-02 16:10:53 +0200

    add api calls on connections

--- departure/src/components/connections/connections.component.jsx
+++ departure/src/components/connections/connections.component.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ConnectionsService from '../../services/connections.service';
+import ConnectionComponent from '../connection/connection.component';
 
 export default function ConnectionsComponent() {
-  return(
-    <span>Connections</span>
+  const [connections, setConnections] = useState(undefined);
+  const connectionsService = new ConnectionsService();
+
+  useEffect(() => {
+    connectionsService.getConnections().then((res) => {
+      setConnections(res.data);
+    })
+      .catch((err) => {
+      console.error(err);
+    });
+  }, []);
+
+  return (
+    <>
+      {
+        connections &&
+        <div id='connection-list'>
+          {
+            connections.map((connection) => {
+              return (
+                connections &&
+                <div key={connection.id}>
+                  <ConnectionComponent connection={connection}></ConnectionComponent>
+                </div>
+              );
+            })
+          }
+        </div>
+      }
+    </>
+
   );
 }
--- departure/src/pages/dashboard/dashboard.page.jsx
+++ departure/src/pages/dashboard/dashboard.page.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import ConnectionsComponent from '../../components/connections/connections.component';
 import SearchComponent from '../../components/search/search.component';
-import { Col, Container, Row } from 'react-bootstrap';
 
-import './dashboard.page.css';
+import './dashboard.page.scss';
 
 export default function DashboardPage() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <ConnectionsComponent className="test" />
-          </Col>
-          <Col>
-            <SearchComponent />
-          </Col>
-        </Row>
-      </Container>
+      <div id='flex'>
+        <div id='connections'>
+          <ConnectionsComponent />
+        </div>
+        <div id='search'>
+          <SearchComponent />
+        </div>
+      </div>
     </>
   );
 }
