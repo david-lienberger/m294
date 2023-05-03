c01551c169a042fa84f44c0512c549d4461f4cb4
e536367 <david.lienberger@sbb.ch>
2023-05-03 21:21:59 +0200

    display delay, render placeholder

--- departure/src/services/connections.service.js
+++ departure/src/services/connections.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class ConnectionsService {
 
   //TODO: remove this with interceptor
-  accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWxpY2VAZXhhbXBsZS5jb20iLCJpYXQiOjE2ODMwNTI3OTIsImV4cCI6MTY4MzA1OTk5Mn0.wcWwExzazH6eTr1Pj1xczxsjHDqq9q0vqYL6n_U0Ucc';
+  accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWxpY2VAZXhhbXBsZS5jb20iLCJpYXQiOjE2ODMxMzk0MjgsImV4cCI6MTY4MzE0NjYyOH0.DaFlIV7TLK3x8a2-k4FxVuEkKT3qtwrR3vIueDdsPQ0';
 
   getConnections() {
     return axios.get('http://localhost:4242/api/connections', {
