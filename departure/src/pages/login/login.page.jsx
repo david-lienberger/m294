aa7fed52ef1a9379e6ef8f4015fc09251f5840d8
e536454 <oliver.werlen@sbb.ch>
2023-05-03 19:05:38 +0200

    login done

--- departure/src/pages/login/login.page.jsx
+++ departure/src/pages/login/login.page.jsx
@@ -16,7 +16,7 @@ class LoginForm extends React.Component {
   handleSubmit = (values, { setSubmitting }) => {
     setTimeout(() => {
       const service = new AuthService();
-      service.getUser({email: values.email, password: values.password});
+      service.getUser(values.email, values.password);
       setSubmitting(false);
     }, 400);
   };
