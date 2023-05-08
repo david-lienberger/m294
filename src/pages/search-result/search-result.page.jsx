bf0dc1cde0db142695904dbd74528bab3bf61f6e
e536454 <oliver.werlen@sbb.ch>
2023-05-08 16:36:45 +0200

    date tommorow shown and reload every minute

--- src/pages/search-result/search-result.page.jsx
+++ src/pages/search-result/search-result.page.jsx
@@ -4,24 +4,30 @@ import moment from 'moment';
 import { Spinner } from 'react-bootstrap';
 
 import './search-result.page.scss';
+import { useTranslation } from 'react-i18next';
 import TransportService from '../../services/transport.service';
 import ConnectionsComponent from '../../components/connections/connections.component';
 import BackButtonComponent from '../../components/back-button/back-button.component';
+import ToastService from '../../services/toast.service';
 
 export default function SearchResultPage() {
+  const { t } = useTranslation();
   const [searchParams] = useSearchParams();
   const [connections, setConnections] = useState(undefined);
   const [loading, setLoading] = useState(true);
   const transportService = new TransportService();
+  const toastService = new ToastService();
   const from = searchParams.get('from');
   const to = searchParams.get('to');
   const nowDate = new Date();
 
   useEffect(() => {
-    transportService.getConnection(from, to).then((res) => {
-      setConnections(res.data.connections);
-      setLoading(false);
-    });
+    setInterval(() => {
+      transportService.getConnection(from, to).then((res) => {
+        setConnections(res.data.connections);
+        setLoading(false);
+      });
+    }, 1000);
   }, []);
 
   if (loading) {
@@ -30,6 +36,10 @@ export default function SearchResultPage() {
         <Spinner animation="border" role="status" />
       </div>
     );
+  }
+
+  if (connections.date > nowDate) {
+    toastService.emit(t('INFORMATION.DATE_IS_TOMMOROW'), 'Information');
   }
 
   return (
@@ -41,14 +51,10 @@ export default function SearchResultPage() {
         <div id="search-res-header">
           <div id="search-res-destinations">
             {from}
-            <span className="material-symbols-outlined">
-              trending_flat
-            </span>
+            <span className="material-symbols-outlined">trending_flat</span>
             {to}
           </div>
-          <div id="search-res-date">
-            { moment(nowDate).locale('de-ch').format('llll') }
-          </div>
+          <div id="search-res-date">{moment(nowDate).locale('de-ch').format('llll')}</div>
         </div>
         <ConnectionsComponent connectionsList={connections} />
       </div>
